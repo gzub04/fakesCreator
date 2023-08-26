8dfa0cfb3760a83fcb7bcd4134eb9b89c9c5f6c8
gzub04 <miclezka@gmail.com>
2023-08-26 14:01:20 +0200

    Updated generation in InitTrainingDocumentsCreator.py

--- run_docker.bat
+++ run_docker.bat
@@ -3,6 +3,7 @@ del /Q .\Data\generated_documents\*
 docker build -t gzub04/fakes_creator .
 docker run --name fakes_container gzub04/fakes_creator
 docker cp fakes_container:/fakesCreator/Data/generated_documents .\Data
-timeout /t 5
+timeout /t 1
 docker rm fakes_container
 docker image prune -f
+PAUSE
