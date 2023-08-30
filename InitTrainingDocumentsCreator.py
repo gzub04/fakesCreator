7ac28e77ebd17e88111dcbe05e9d851c295b9429
gzub04 <gzub04@users.noreply.github.com>
2023-08-30 13:41:44 +0200

    Refactor InitTrainingDocumentsCreator.py to a separate class FakeDataCreator and leave InitTrainingDocumentsCreator only for creating training documents. Also slightly changed source file as phone number was added.

--- InitTrainingDocumentsCreator.py
+++ InitTrainingDocumentsCreator.py
@@ -1,7 +1,5 @@
+import fakeDataCreator
 from docx import Document
-from random import randint, choice
-import datetime
-import sys
 
 
 SOURCE_FILES = "Data/sources"
@@ -9,156 +7,55 @@ INPUT_DOCUMENT = f"{SOURCE_FILES}/hospitalInformationSheet.docx"
 OUTPUT_DIRECTORY = "Data/generated_documents"
 
 
-def err_exit(*args):
-    print('Error: '*args, file=sys.stderr)
-    exit(1)
+def produce_new_init_document(output_name):
+    information_sheet = Document(INPUT_DOCUMENT)
+
+    fake_data = fakeDataCreator.FakeDataCreator()
+    person = fake_data.create_person()
+    address = fake_data.create_address()
+    dates = fake_data.create_dates()
+    phone_number = fake_data.create_phone_number()
+
+    birthday = person['birthdate']
+    start_date = dates['start_date']
+    end_date = dates['end_date']
+
+    paragraph = information_sheet.paragraphs[3]
+    paragraph.runs[6].text = f"{person['first_name']} {person['last_name']}"
+    paragraph.runs[11].text = f"{person['pesel']}"
+
+    paragraph = information_sheet.paragraphs[4]
+    paragraph.runs[3].text = f"Kraków, {end_date.strftime('%Y-%m-%d')}"
+
+    paragraph = information_sheet.paragraphs[10]
+    paragraph.runs[2].text = f"{person['first_name']} {person['last_name']}"
+    paragraph.runs[7].text = f"{person['pesel']}"
+
+    paragraph = information_sheet.paragraphs[11]
+    paragraph.runs[3].text = f"{birthday.strftime('%Y-%m-%d')}"
+    paragraph.runs[6].text = f" {phone_number}"
+    paragraph.runs[13].text = f"{person['sex']}"
+
+    paragraph = information_sheet.paragraphs[12]
+    paragraph.runs[0].text = f"Adres zamieszkania: {address['street']}, {address['city']}"
+
+    paragraph = information_sheet.paragraphs[13]
+    paragraph.runs[0].text = f"Data i godzina przyjęcia pacjenta: {start_date.strftime('%Y-%m-%d')}, " \
+                             f"{start_date.strftime('%H:%M')}, " \
+                             f"data i godzina wypisu pacjenta: {end_date.strftime('%y-%m-%d')}, " \
+                             f"{end_date.strftime('%H:%M')}"
+
+    information_sheet.save(f"{OUTPUT_DIRECTORY}/{output_name}")
 
 
-class HospitalInformationSheetCreator:
-    name = str()
-    sex = str()
-    pesel = str()
-    address = str()
-    phone_number = str()
-    birthdate = None
-    arrival_date = None
-    discharge_date = None
-
-    def _create_name(self):
-        if self.sex == 'M':
-            file_first_names = open(f'{SOURCE_FILES}/male_firstnames.csv', 'r', encoding='utf-8')
-            file_last_names = open(f'{SOURCE_FILES}/male_lastnames.csv', 'r', encoding='utf-8')
-        else:
-            file_first_names = open(f'{SOURCE_FILES}/female_firstnames.csv', 'r', encoding='utf-8')
-            file_last_names = open(f'{SOURCE_FILES}/female_lastnames.csv', 'r', encoding='utf-8')
-
-        first_names = file_first_names.readlines()
-        last_names = file_last_names.readlines()
-
-        file_first_names.close()
-        file_last_names.close()
-
-        self.name = choice(first_names).rstrip('\n').capitalize() + ' ' + choice(last_names).rstrip('\n').capitalize()
-
-    def _create_dates(self):
-        """
-        Returns list of length 5:
-        [0] - birthdate
-        [1] - arrival at hospital date
-        [2] - arrival at hospital hour
-        [3] - discharge date
-        [4] - discharge hour
-        """
-
-        # generate birthdate date from 23 to 60 years ago
-        self.birthdate = datetime.date.today() - datetime.timedelta(days=(randint(23, 60) * 365))
-
-        # Generate a random arrival date between 7 days and 20 years ago
-        self.arrival_date = datetime.datetime.now() - datetime.timedelta(days=randint(7, 365 * 20))
-        self.arrival_date = self.arrival_date.replace(hour=randint(0, 23), minute=randint(0, 59))
-
-        # Generate random discharge date between 1 and 7 days after the arrival date
-        self.discharge_date = self.arrival_date + datetime.timedelta(days=randint(1, 5))
-        self.discharge_date = self.discharge_date.replace(hour=randint(10, 16), minute=choice([0, 30]))
-
-        return [birthdate, arrival_date.strftime('%Y-%m-%d'), arrival_date.strftime('%H:%M'),
-                discharge_date.strftime('%Y-%m-%d'), discharge_date.strftime('%H:%M')]
-
-    def _create_address(self):
-        addresses_file = open(f'{SOURCE_FILES}/addresses.csv', 'r', encoding='utf-8')
-        addresses = addresses_file.readlines()
-        addresses_file.close()
-
-        self.address = choice(addresses).rstrip('\n') + ' ' + str(randint(1, 100))
-        if randint(1, 100) > 55:  # chance for someone to live in a flat
-            self.address += '/' + randint(1, 80)
-
-    def _create_phone_number(self):
-        self.phone_number = str(randint(5, 8))  # first digit
-        self.phone_number += ''.join([str(randint(0, 9)) for _ in range(8)])  # remaining digits
-
-    def _create_pesel(self):
-        if self.birthdate is None:
-            err_exit('Birthdate is not set when _create_pesel was called')
-
-        p_year = str(self.birthdate.year)[-2:]  # last two digits of year
-
-        p_month = str()
-        if 1800 <= self.birthdate.year < 1900:
-            p_month = str(self.birthdate.month + 80)
-        elif self.birthdate.year < 2000:
-            p_month = str(self.birthdate.month)
-        elif self.birthdate.year < 2100:
-            p_month = str(self.birthdate.month + 20)
-        else:
-            err_exit('Birth year is not between 1800-2100')
-
-        p_day = str(self.birthdate.day)
-
-        p_ordinal_number = ''.join([str(randint(0, 9)) for _ in range(3)])
-        if self.sex == 'M':
-            p_ordinal_number += str(randint(0, 4) * 2 + 1)
-        elif self.sex == 'F':
-            p_ordinal_number += str(randint(0, 4) * 2)
-        else:
-            err_exit('Unknown sex')
-
-        pesel_tmp = p_year + p_month + p_day + p_ordinal_number
-
-        factors = [1, 3, 7, 9]
-        result_sum = 0
-        for i in range(10):
-            digit = pesel_tmp[i]
-            result_sum += digit * factors[i % 4]
-        p_control_number = result_sum % 10
-
-        self.pesel = pesel_tmp + p_control_number
-
-    def produce_new_file(self):
-        information_sheet = Document(INPUT_DOCUMENT)
-        self.sex = choice(['K', 'M'])
-        self._create_pesel()
-        self._create_name()
-        self._create_dates()
-        self._create_address()
-        self._create_phone_number()
-
-        paragraph = information_sheet.paragraphs[3]
-        paragraph.runs[6].text = f'{self.name}    '
-        paragraph.runs[11].text = f'{self.pesel}'
-
-        paragraph = information_sheet.paragraphs[4]
-        paragraph.runs[3].text = f"Kraków, {self.discharge_date.strftime('%Y-%m-%d')}"
-        # TODO: dodać miasta do adresów
-
-        paragraph = information_sheet.paragraphs[10]
-        paragraph.runs[2].text = f'{new_name}'
-        paragraph.runs[7].text = f'{new_pesel}'
-
-        paragraph = information_sheet.paragraphs[11]
-        paragraph.runs[3].text = f'{new_dates[0]}'
-        paragraph.runs[11].text = f'{new_sex}'
-
-        paragraph = information_sheet.paragraphs[12]
-        paragraph.runs[0].text = f'Adres zamieszkania: {new_address}'
-
-        paragraph = information_sheet.paragraphs[13]
-        paragraph.runs[0].text = f'Data i godzina przyjęcia pacjenta: {new_dates[1]}, {new_dates[2]}, ' \
-                                 f'data i godzina wypisu pacjenta: {new_dates[3]}, {new_dates[4]}'
-
-        information_sheet.save(f'{OUTPUT_DIRECTORY}/edited.docx')
-
-
-
-def create_xml(information_sheet):
-    with open('HospitalInformationSheet.xml', 'w') as f:
+def create_xml():
+    information_sheet = Document(INPUT_DOCUMENT)
+    with open('HospitalInformationSheet.xml', 'w', encoding='utf-8') as f:
         f.write(information_sheet._element.xml)
-
-
-def print_docx(information_sheet):
-    i = 0
-    for paragraph in information_sheet.paragraphs:
-        print(f'{i}: {paragraph.text}')
-        i += 1
-
-
+#
+#
+# def print_docx(information_sheet):
+#     i = 0
+#     for paragraph in information_sheet.paragraphs:
+#         print(f'{i}: {paragraph.text}')
+#         i += 1
---
+++ fakeDataCreator.py
@@ -0,0 +1,155 @@
+from random import randint, choice
+import datetime
+import sys
+
+
+SOURCE_FILES = "Data/sources"
+
+
+def err_exit(*err_msg):
+    print(f'Error: {err_msg}', file=sys.stderr)
+    exit(1)
+
+
+class FakeDataCreator:
+    def __init__(self):
+        try:
+            with open(f'{SOURCE_FILES}/male_firstnames.csv', 'r', encoding='utf-8') as f_male_first_names, \
+                    open(f'{SOURCE_FILES}/male_lastnames.csv', 'r', encoding='utf-8') as f_male_last_names, \
+                    open(f'{SOURCE_FILES}/female_firstnames.csv', 'r', encoding='utf-8') as f_female_first_names, \
+                    open(f'{SOURCE_FILES}/female_lastnames.csv', 'r', encoding='utf-8') as f_female_last_names, \
+                    open(f'{SOURCE_FILES}/cities.csv', 'r', encoding='utf-8') as f_cities, \
+                    open(f'{SOURCE_FILES}/street_names.csv', 'r', encoding='utf-8') as f_streets:
+
+                self.male_first_names = f_male_first_names.readlines()
+                self.male_last_names = f_male_last_names.readlines()
+                self.female_first_names = f_female_first_names.readlines()
+                self.female_last_names = f_female_last_names.readlines()
+
+                self.street_names = f_streets.readlines()
+                self.cities_data = []   # list in format [(city, population)]
+                for line in f_cities:
+                    city, population = line.strip().split(';')
+                    self.cities_data.append((city, int(population)))
+
+
+        except FileNotFoundError:
+            print("File Not Found Error: Missing csv files in Data/sources.")
+            exit(1)
+        except PermissionError:
+            print("Permission Error: Can't access csv files in Data/sources.")
+            exit(1)
+        except Exception as e:
+            print(f"An error occurred while opening csv files: {str(e)}")
+            exit(1)
+
+    def create_person(self):
+        """
+        Creates a dictionary with randomly generated person's data.
+        :return: Dictionary with keys: first_name, last_name, sex, birthdate and PESEL.
+        birthdate is datetime datatype, all others are strings
+        """
+        person = {'sex': choice(['M', 'F'])}
+
+
+        if person['sex'] == 'M':
+            person['first_name'] = choice(self.male_first_names).rstrip('\n').capitalize()
+            person['last_name'] = choice(self.male_last_names).rstrip('\n').capitalize()
+        else:
+            person['first_name'] = choice(self.female_first_names).rstrip('\n').capitalize()
+            person['last_name'] = choice(self.female_last_names).rstrip('\n').capitalize()
+
+        # generate birthdate date from 23 to 60 years ago
+        person['birthdate'] = datetime.date.today() - datetime.timedelta(days=(randint(23, 60) * 365))
+
+
+        # PESEL
+        p_year = str(person['birthdate'].year)[-2:]  # last two digits of year
+
+        p_month = ''
+        if 1800 <= person['birthdate'].year < 1900:
+            p_month = str(person['birthdate'].month + 80)
+        elif person['birthdate'].year < 2000:
+            if person['birthdate'].month < 10:
+                p_month = '0'
+            p_month += str(person['birthdate'].month)
+        elif person['birthdate'].year < 2100:
+            p_month = str(person['birthdate'].month + 20)
+        else:
+            err_exit('Birth year is not between 1800-2100')
+
+        if person['birthdate'].day < 10:
+            p_day = '0' + str(person['birthdate'].day)
+        else:
+            p_day = str(person['birthdate'].day)
+
+        p_ordinal_number = ''.join([str(randint(0, 9)) for _ in range(3)])
+        if person['sex'] == 'M':
+            p_ordinal_number += str(randint(0, 4) * 2 + 1)
+        else:
+            p_ordinal_number += str(randint(0, 4) * 2)
+
+        pesel_wo_last_number = p_year + p_month + p_day + p_ordinal_number
+
+        factors = [1, 3, 7, 9]
+        result_sum = 0
+        for i in range(10):
+            digit = int(pesel_wo_last_number[i])
+            result_sum += digit * factors[i % 4]
+        p_control_number = result_sum % 10
+
+        person['pesel'] = pesel_wo_last_number + str(p_control_number)
+
+        return person
+
+    def create_address(self):
+        """
+        Creates a dictionary containing random address
+        :return: Dictionary with keys: city, street.
+        Street contains both street name and house number with apartment number (if apartment number is generated)
+        """
+        address = dict()
+
+        total_population = sum(population for _, population in self.cities_data)
+        # Chooses city with higher chance for the city to be more populated
+        random_number = randint(1, total_population)
+        cumulative_population = 0
+        for city, population in self.cities_data:
+            cumulative_population += population
+            if cumulative_population >= random_number:
+                address['city'] = city
+                break
+
+        address['street'] = choice(self.street_names).rstrip('\n') + ' ' + str(randint(1, 100))
+
+        if randint(1, 100) > 55:  # chance for someone to live in a flat
+            address['street'] += '/' + str(randint(1, 100))
+        return address
+
+    @staticmethod
+    def create_dates():
+        """
+        Returns list of length 5:
+        [0] - birthdate
+        [1] - arrival at hospital date / start date
+        [2] - arrival at hospital hour / start hour
+        [3] - discharge date / end date
+        [4] - discharge hour / end hour
+        """
+        dates = dict()
+
+        # Generate a random start date between 7 days and 20 years ago
+        dates['start_date'] = datetime.datetime.now() - datetime.timedelta(days=randint(7, 365 * 20))
+        dates['start_date'] = dates['start_date'].replace(hour=randint(0, 23), minute=randint(0, 59))
+
+        # Generate random discharge date between 1 and 7 days after the arrival date
+        dates['end_date'] = dates['start_date'] + datetime.timedelta(days=randint(1, 5))
+        dates['end_date'] = dates['end_date'].replace(hour=randint(10, 16), minute=choice([0, 30]))
+
+        return dates
+
+    @staticmethod
+    def create_phone_number():
+        phone_number = str(randint(5, 8))  # first digit
+        phone_number += ''.join([str(randint(0, 9)) for _ in range(8)])  # remaining digits
+        return phone_number
