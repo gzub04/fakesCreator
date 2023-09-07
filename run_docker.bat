e9fb4023bd8a4565ea5b7c8a5e436b4f0f8ed1f0
gzub04 <gzub04@users.noreply.github.com>
2023-09-07 20:28:18 +0200

    Copies whole /data folder

--- run_docker.bat
+++ run_docker.bat
@@ -1,7 +1,7 @@
 del /Q .\Data\generated_documents\*
 docker build -t gzub04/fakes_creator .
 docker run --name fakes_container gzub04/fakes_creator
-docker cp fakes_container:/fakesCreator/data/generated_documents .\data
+docker cp fakes_container:/fakesCreator/data .\
 timeout /t 1
 docker rm fakes_container
 docker image prune -f
