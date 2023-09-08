7f72dcababf0733a4bbb014e2ef4426ab26c441f
gzub04 <gzub04@users.noreply.github.com>
2023-09-08 14:06:49 +0200

    Add image rotation for scans and photos

--- image_distorting.py
+++ image_distorting.py
@@ -5,13 +5,15 @@ from PIL import Image, ImageEnhance  # , ImageFilter
 MAX_SCAN_ROTATION = 1.8
 MAX_PHOTO_ROTATION = None
 
-PAPER_PATH = "data/sources/paper_textures"
+PAPER_PATH = "data/sources/textures"
+BACKGROUND_PATH = 'data/sources/textures/img_background.jpg'
 
 
 class ImageDistorting:
     def __init__(self):
         self.paper_textures = []
 
+        # paper textures
         for texture_num in range(1, 7):
             path_to_img = f"{PAPER_PATH}/paper_texture_{texture_num}.jpg"
             try:
@@ -24,6 +26,17 @@ class ImageDistorting:
             except PermissionError:
                 print(f"Permission Error: Can't access {path_to_img}")
                 exit(1)
+
+        # background texture
+        try:
+            with Image.open(BACKGROUND_PATH) as background_texture:
+                self.background = background_texture
+        except FileNotFoundError:
+            print(f"File Not Found Error: Missing {BACKGROUND_PATH}")
+            exit(1)
+        except PermissionError:
+            print(f"Permission Error: Can't access {BACKGROUND_PATH}")
+            exit(1)
 
     def _apply_paper_texture(self, image):
         if random.randint(1, 10) == 10:
@@ -39,12 +52,49 @@ class ImageDistorting:
 
         return image
 
-    @staticmethod
-    def _rotate_img(pil_image, max_rotation):
-        rotation = random.uniform(-max_rotation, max_rotation)
-        pil_image = pil_image.rotate(rotation, fillcolor=(255, 255, 255), resample=Image.BICUBIC)
+    def rotate_img(self, pil_image, img_type):
+        """
+        rotates image by a random amount and adds background based on image type
+        :param pil_image: image in PIL format
+        :param img_type: string 'scan' or 'photo'
+        :return: returns rotated image in PIL format
+        """
 
-        return pil_image
+        if img_type == 'scan':
+            max_rotation = 2
+            rotation = random.uniform(-max_rotation, max_rotation)
+            output_image = pil_image.rotate(rotation, expand=True, fillcolor=(255, 255, 255, 255))
+        elif img_type == 'photo':
+            max_rotation = 4
+            rotation = random.uniform(-max_rotation, max_rotation)
+            # Rotate and expand the image, filling empty space with black
+            rotated_pil_image = pil_image.rotate(rotation, expand=True, fillcolor=(0, 0, 0, 0))
+            # Create a mask
+            mask = rotated_pil_image.convert('L').point(lambda x: 255 if x > 0 else 0)
+            # resize the background and paste
+            width, height = rotated_pil_image.size
+            extend_image = 64
+            width += extend_image
+            height += extend_image
+            output_image = self.background.resize((width, height))
+            output_image.paste(rotated_pil_image, (32, 32), mask)
+
+            rotated_pil_image.close()
+        else:
+            print(f"Error: unknown img_type \"{img_type}\" in rotate_img function")
+            exit(1)
+
+        # note changes in image
+        in_width, in_height = pil_image.size
+        out_width, out_height = output_image.size
+        changes = {
+            'rotation': rotation,
+            'width_increase': out_width - in_width,
+            'height_increase': out_height - in_height
+        }
+
+        return output_image, changes
+
 
     @staticmethod
     def photo_distortion(pil_image):
